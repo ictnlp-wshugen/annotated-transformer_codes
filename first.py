83d9e960e087b8c376277c9a27c7c79a3e20929a
Wang Shugen <wangshugen@ict.ac.cn>
2019-01-08 11:01:34 +0800

    init commit
    Signed-off-by: Wang Shugen <wangshugen@ict.ac.cn>

---
+++ first.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# date: 2018-12-02 17:01
+import numpy
+import torch
+from torch.autograd import Variable
+
+from transformer.batch import Batch
+from transformer.flow import make_model
+from transformer.flow import run_epoch
+from transformer.greedy import greedy_decode
+from transformer.label_smoothing import LabelSmoothing
+from transformer.noam_opt import NoamOpt
+
+
+def data_gen(V, batch, nbatches):
+    """
+    Generate random data for a src-tgt copy task.
+    """
+    for i in range(nbatches):
+        data = torch.from_numpy(numpy.random.randint(1, V, size=(batch, 10)))
+        data[:, 0] = 1
+        src = Variable(data, requires_grad=False)
+        tgt = Variable(data, requires_grad=False)
+        yield Batch(src, tgt, 0)
+
+
+class SimpleLossCompute(object):
+    """
+    A simple loss compute and train function.
+    """
+
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1)) / norm
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.item() * norm
+
+
+if __name__ == '__main__':
+    # Greedy decoding
+    V = 11
+    criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+    model = make_model(V, V, n=2)
+    model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
+                        torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+    for epoch in range(10):
+        model.train()
+        run_epoch(data_gen(V, 30, 20), model, SimpleLossCompute(model.generator, criterion, model_opt))
+        model.eval()
+        print(run_epoch(data_gen(V, 30, 5), model, SimpleLossCompute(model.generator, criterion, None)))
+
+    model.eval()
+    src = Variable(torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]))
+    src_mask = Variable(torch.ones(1, 1, 10))
+    print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
---
+++ realworld.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# date: 2018-12-02 20:54
+import spacy
+import torch
+import torch.nn as nn
+from torchtext import data, datasets
+
+from transformer.flow import make_model, batch_size_fn, run_epoch
+from transformer.greedy import greedy_decode
+from transformer.label_smoothing import LabelSmoothing
+from transformer.multi_gpu_loss_compute import MultiGPULossCompute
+from transformer.my_iterator import MyIterator, rebatch
+from transformer.noam_opt import NoamOpt
+
+# Note: not runnable for data reason (the data archive is missing) !!!
+
+# GPUs to use
+devices = [0, 1]
+
+BOS_WORD = '<s>'
+EOS_WORD = '</s>'
+BLANK_WORD = '<blank>'
+
+
+def tokenize_de(text):
+    return [tok.text for tok in spacy_de.tokenizer(text)]
+
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+TGT = data.Field(tokenize=tokenize_en, init_token=BOS_WORD,
+                 eos_token=EOS_WORD, pad_token=BLANK_WORD)
+
+if True:
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
+
+    MAX_LEN = 100
+    train, val, test = datasets.IWSLT.splits(exts=('.de', 'en'), fields=(SRC, TGT),
+                                             filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and len(
+                                                 vars(x)['trg']) <= MAX_LEN)
+
+    MIN_FREQ = 2
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+    pad_idx = TGT.vocab.stoi[BLANK_WORD]
+    model = make_model(len(SRC.vocab), len(TGT.vocab), n=6)
+    model.cuda()
+    criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
+    criterion.cuda()
+    BATCH_SIZE = 12000
+    train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0, repeat=False,
+                            sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=True)
+    valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0, repeat=False,
+                            sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=False)
+    model_par = nn.DataParallel(model, device_ids=devices)
+
+    model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
+                        torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+    for epoch in range(10):
+        model_par.train()
+        run_epoch((rebatch(pad_idx, b) for b in train_iter), model_par,
+                  MultiGPULossCompute(model.generator, criterion, devices=devices, opt=model_opt))
+
+        model_par.eval()
+        loss = run_epoch((rebatch(pad_idx, b) for b in valid_iter), model_par,
+                         MultiGPULossCompute(model.generator, criterion, devices=devices, opt=None))
+        print(loss)
+else:
+    model = torch.load('iwslt.pt')
+
+for i, batch in enumerate(valid_iter):
+    src = batch.src.transpose(0, 1)[:1]
+    src_mask = (src != SRC.vocab.stoi[BLANK_WORD]).unsqueeze(-2)
+    out = greedy_decode(model, src, src_mask, max_len=60, start_symbol=TGT.vocab.stoi[BOS_WORD])
+    print('Translation:', end='\t')
+    for i in range(1, out.size(1)):
+        sym = TGT.vocab.itos[out[0, i]]
+        if sym == EOS_WORD:
+            break
+        print(sym, end=' ')
+    print()
+    print('Target:', end='\t')
+    for i in range(batch.trg.size(0)):
+        sym = TGT.vocab.itos(batch.trg.data[i, 0])
+        if sym == EOS_WORD:
+            break
+        print(sym, end=' ')
+    print()
+    break
